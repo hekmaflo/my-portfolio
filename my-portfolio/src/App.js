9457f10b50e3687ab6d8e5abe290192bcb70f691
Hector Flores <106941903+hekmaflo@users.noreply.github.com>
2023-02-20 19:16:00 -0600

    updated layout

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -5,12 +5,18 @@ function App() {
   return (
     <div className="App">
       <div className="Main">
-        <div className="Nav">
-          <h4>home</h4>
-        </div>
         <div className="Intro-card">
+          <div className="Nav">
+            <h4>home</h4>
+            <h4>portfolio</h4>
+            <h4>About</h4>
+            <h4>contact me</h4>
+          </div>
           <h1>HECTOR FLORES</h1>
           <h1>Software Engineer</h1>
+        </div>
+        <div className="Projects">
+          <h2>Projects</h2>
         </div>
       </div>
     </div>
