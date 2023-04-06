1b330a0549370af1a94c31eaec6509497b3d4d33
Hector Flores <106941903+hekmaflo@users.noreply.github.com>
2023-04-05 22:03:54 -0500

    scrapped to start new portfolio

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -4,21 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="Main">
-        <div className="Intro-card">
-          <div className="Nav">
-            <h4>home</h4>
-            <h4>portfolio</h4>
-            <h4>About</h4>
-            <h4>contact me</h4>
-          </div>
-          <h1>HECTOR FLORES</h1>
-          <h1>Software Engineer</h1>
-        </div>
-        <div className="Projects">
-          <h2>Projects</h2>
-        </div>
-      </div>
+      <h1>hi</h1>
     </div>
   );
 }
