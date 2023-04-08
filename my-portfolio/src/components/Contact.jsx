7110afc6acad3281d065fc887faf028254540554
Hector Flores <106941903+hekmaflo@users.noreply.github.com>
2023-04-08 16:43:39 -0500

    fixed alignment for Contacts

--- my-portfolio/src/components/Contact.jsx
+++ my-portfolio/src/components/Contact.jsx
@@ -1,25 +1,43 @@
 import React from "react";
-
+import phone from "./assets/phone.png";
+import email from "./assets/email.png";
+import social from "./assets/social.png";
 const Contact = () => {
   return (
     <div className="More">
       <div className="all-info">
         <div className="contact-info">
-          <h4 className="contact-info-spacing">My Number</h4>
-          <p className="contact-info-spacing">(956)-537-8972</p>
+          <div className="contact-logo-box">
+            <img className="contact-logo" src={phone} alt="" />
+          </div>
+          <div>
+            <h4 className="contact-info-spacing">My Number</h4>
+            <p className="contact-info-spacing">(956)-537-8972</p>
+          </div>
         </div>
         <div className="contact-info">
-          <h4 className="contact-info-spacing">Email</h4>
-          <p className="contact-info-spacing">hmflores88@gmail.com</p>
+          <div className="contact-logo-box">
+            <img className="contact-logo" src={email} alt="" />
+          </div>
+          <div>
+            <h4 className="contact-info-spacing">Email</h4>
+            <p className="contact-info-spacing">hmflores88@gmail.com</p>
+          </div>
         </div>
         <div className="contact-info">
-          <h4 className="contact-info-spacing">LinkedIn</h4>
-          <p className="contact-info-spacing">www.linkedin.com/hmflores88</p>
+          <div className="contact-logo-box">
+            <img className="contact-logo" src={social} alt="" />
+          </div>
+          <div>
+            <h4 className="contact-info-spacing">LinkedIn</h4>
+            <p className="contact-info-spacing">www.linkedin.com/hmflores88</p>
+          </div>
         </div>
       </div>
       <div className="contact-form">
-        <h3>Shoot me</h3>
+        <h3>Send me a message</h3>
         <div className="name-email-box">
+          <label htmlFor="">Name</label>
           <input
             className="contact-input-user-info "
             type="text"
@@ -27,6 +45,7 @@ const Contact = () => {
             id=""
             placeholder="Full Name"
           />
+          <p>required</p>
           <input
             className="contact-input-user-info "
             type="text"
--- my-portfolio/src/components/Nav.jsx
+++ my-portfolio/src/components/Nav.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import React from "react";
 import mug from "./assets/mug.png";
 const Nav = () => {
   return (
     <div className="Navbar">
-      <img src={mug} alt="mug" />
+      <img className="logo-small" src={mug} alt="mug" />
+      <p className="smol-text">*sip sip*</p>
     </div>
   );
 };
